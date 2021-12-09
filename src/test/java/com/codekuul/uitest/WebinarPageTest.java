19f48f6caf05426b138258a858e3a91ffb288d30
pravind1884 <codekuul@gmail.com>
2021-12-09 14:09:23 +0530

    today commit

---
+++ src/test/java/com/codekuul/uitest/WebinarPageTest.java
@@ -0,0 +1,14 @@
+package com.codekuul.uitest;
+
+import org.testng.annotations.Test;
+
+public class WebinarPageTest {
+	
+	
+	@Test
+	public void validateTitleOfPage() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
